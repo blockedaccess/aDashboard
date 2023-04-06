6f820fd6e3c8fe6168abaf339a731c8f886f864f
leon.kim.0509@gmail.com <leon.kim.0509@gmail.com>
2023-04-06 01:31:24 -0400

    push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -57,7 +57,7 @@ function App() {
 
           if (profileObj) {
               const response = await fetch(
-                  "http://localhost:8080/api/v1/users",
+                  "https://adashboard.onrender.com/api/v1/users",
                   {
                       method: "POST",
                       headers: { "Content-Type": "application/json" },
@@ -127,7 +127,7 @@ function App() {
           <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
           <RefineSnackbarProvider>
               <Refine
-                  dataProvider={dataProvider("http://localhost:8080/api/v1")}
+                  dataProvider={dataProvider("https://adashboard.onrender.com/api/v1")}
                   notificationProvider={notificationProvider}
                   ReadyPage={ReadyPage}
                   catchAll={<ErrorComponent />}
